82729e407336e3bd148ff6ea7f5bf4444415c9eb
Genevieve Costa <togenny@yahoo.com>
2017-10-03 20:03:03 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,26 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  hash = {}
+    languages.each do |func_oo_key, prog_name_key|
+      prog_name_key.each do |type_key, type_name_key|
+        type_name_key.each do |type_name, string|
+          if type_key == :javascript
+            prog_name = type_key
+            type = type_name
+            name = string
+            style_s = [:oo, :functional]
+            hash[prog_name] = {type => name, :style => style_s}
+          else
+        prog_name = type_key
+        type = type_name
+        name = string
+        style_s = [func_oo_key]
+        hash[prog_name] = {type => name, :style => style_s}
+        # binding.pry
+
+end
+end
+end
+end
+return hash
 end
